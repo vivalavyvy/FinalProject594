9f02cabe153e06cc2ad26bc7fa6368d3c33dcc68
steveBaca <smbaca99@gmail.com>
2020-12-08 18:21:20 -0500

    further work on logger

--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,11 +1,68 @@
 package edu.upenn.cit594.logging;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 public class Logger {
-	
-	
-	//private constructor because it is a singleton implementation 
-	private Logger() {
-		
+
+	private String fileName;
+	private PrintWriter out;
+
+	// 1. private constuctor
+	private Logger(String fileName) {
+		// TODO Auto-generated constructor stub
+		this.fileName = fileName;
+
+		try {
+
+			PrintWriter writer = new PrintWriter(fileName);
+			writer.print("");
+			writer.close();
+			out = new PrintWriter(new File(fileName));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @author stevebaca
+	 * @since 12/08
+	 * @param Args
+	 */
+	public void writeRunTimeArgs(String[] args) {
+
+		for (int i = 0; i < args.length; i++) {
+
+			out.println(System.currentTimeMillis() + " " + args[i]);
+			out.flush();
+
+		}
+	}
+
+	/**
+	 * @author stevebaca
+	 * @since 12/08
+	 * @param test
+	 */
+	public void openedForReading(String fileName) {
+
+		out.println(System.currentTimeMillis() + " " + fileName);
+		out.flush();
+	}
+
+	// 2. singleton instance
+	// I was told by a TA to hard code the name of the logger file into this class
+	private static Logger logger = null;
+
+	// 3. singleton accessor method
+
+	public static Logger getInstance(String fileName) {
+		if (logger == null) {
+			logger = new Logger(fileName);
+		}
+
+		return logger;
 	}
 
 }
