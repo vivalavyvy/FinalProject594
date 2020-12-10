4204249f6d99f84de1ddb1500401a44781829c4a
steveBaca <smbaca99@gmail.com>
2020-12-09 19:30:17 -0500

    updating file system, created folder lib to hold jars

--- src/edu/upenn/cit594/processor/ResidentialMarketValuesPerCapita.java
+++ src/edu/upenn/cit594/processor/ResidentialMarketValuesPerCapita.java
@@ -1,82 +1,80 @@
 package edu.upenn.cit594.processor;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import edu.upenn.cit594.data.Properties;
-import edu.upenn.cit594.datamanagement.csvReader;
 
 public class ResidentialMarketValuesPerCapita {
-	
-	public String zipCode;
-	public csvReader csvReader;
-	
-	public ResidentialMarketValuesPerCapita(String zipCode) {
-		this.zipCode = zipCode;
-		csvReader = new csvReader(properties, parking, population);
-	}
+
+	private static // memoization
+	HashMap<String, Double> memoizationTracker = new HashMap<String, Double>();
 	
 	
+
 	/**
 	 * @author stevebaca
 	 * @since 12/9
 	 * @param zipCode
 	 */
-	private int findPopulationForZipCode(String zipCode,Map<String, Integer> populationByZipCode) {
-		
-		
+	private static int findPopulationForZipCode(String zipCode, Map<String, Integer> populationByZipCode) {
+
 		for (String zipCodeIterator : populationByZipCode.keySet()) {
-			
-			if(zipCodeIterator.equals(zipCode)) {
-				
+
+			if (zipCodeIterator.equals(zipCode)) {
+
 				return populationByZipCode.get(zipCode);
-				
+
 			}
-			
+
 		}
-		
-		
+
 		return 0;
 	}
-	
+
 	/**
 	 * @author stevebaca
 	 * @since 12/9
 	 * @param zipCode
 	 * @return
 	 */
-	private int findSumOfMarketValueForZipCode(String zipCode, List<Properties> propertiesList) {
-		
+	private static int findSumOfMarketValueForZipCode(String zipCode, List<Properties> propertiesList) {
+
 		int sumOfMarketValue = 0;
-		
-		for (Properties p: propertiesList) {
-			
-			if(p.getZipCode().equals(zipCode)) {
-				
+
+		for (Properties p : propertiesList) {
+
+			if (p.getZipCode().equals(zipCode)) {
+
 				sumOfMarketValue += Integer.parseInt(p.getMarketValue());
 			}
-			
+
 		}
-		
+
 		return sumOfMarketValue;
-		
+
 	}
-	
+
 	/**
 	 * @author stevebaca
 	 * @since 12/9
 	 * @return
 	 */
-	public double calculateTotalResidentialMarketValuePerCapita(String zipCode, List<Properties> propertiesList, Map<String, Integer> populationByZipCode) {
-		
-		double totalResidentialMarketValuePerCapita = findSumOfMarketValueForZipCode(zipCode, propertiesList)/findPopulationForZipCode(zipCode, populationByZipCode);
-		
-		
+	public static double calculateTotalResidentialMarketValuePerCapita(String zipCode, List<Properties> propertiesList,
+			Map<String, Integer> populationByZipCode) {
+
+		//memoization
+		if (memoizationTracker.containsValue(zipCode)) {
+			return memoizationTracker.get(zipCode);
+		}
+
+		double totalResidentialMarketValuePerCapita = findSumOfMarketValueForZipCode(zipCode, propertiesList)
+				/ findPopulationForZipCode(zipCode, populationByZipCode);
+
+		memoizationTracker.put(zipCode, totalResidentialMarketValuePerCapita);
+
 		return totalResidentialMarketValuePerCapita;
 	}
-	
-	
-	
-	
 
 }
